47ccb18734d35aaaa209eaae8623b0893ba72a4d
FdeFabricio <fabriciocomf@gmail.com>
2016-07-04 13:09:20 -0300

    histórico de mensagens

--- servent.py
+++ servent.py
@@ -58,18 +58,21 @@ while True:
         for n in neighbors:
             print "Eu enviei para o vizinho", n
             s.sendto(pack("!HHBBBBHI20s",2,3,ip1, ip2, ip3, ip4, portClient, SEQ_NO, key), n)
-            SEQ_NO += 1
+        history.append((ip1, ip2, ip3, ip4, portClient, SEQ_NO, key))
+        SEQ_NO += 1
             
     if (tipo == 2):
         print "QUERY"
         tipo, ttl, ip1, ip2, ip3, ip4, port, seqNo, key = unpack("!HHBBBBHI20s", data)
-        key = key.rstrip('\0')
-        if key in chaves:
-            print "Eu tenho a chave", key
-            ip = str(ip1)+"."+str(ip2)+"."+str(ip3)+"."+str(ip4)
-            s.sendto(pack("!H100s", 3, key+"\t"+valores[chaves.index(key)]), (ip, port))
-        if (ttl > 1):
-            for n in neighbors:
-                if address != n:
-                    print "Eu enviei para o vizinho", n
-                    s.sendto(pack("!HHBBBBHI20s",2,ttl-1,ip1, ip2, ip3, ip4, portClient, SEQ_NO, key), n)
+        if (ip1, ip2, ip3, ip4, port, seqNo, key) not in history:
+            history.append((ip1, ip2, ip3, ip4, port, seqNo, key))
+            key = key.rstrip('\0')
+            if key in chaves:
+                print "Eu tenho a chave", key
+                ip = str(ip1)+"."+str(ip2)+"."+str(ip3)+"."+str(ip4)
+                s.sendto(pack("!H100s", 3, key+"\t"+valores[chaves.index(key)]), (ip, port))
+            if (ttl > 1):
+                for n in neighbors:
+                    if address != n:
+                        print "Eu enviei para o vizinho", n
+                        s.sendto(pack("!HHBBBBHI20s",tipo,ttl-1,ip1, ip2, ip3, ip4, port, seqNo, key), n)
